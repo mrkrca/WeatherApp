4984e2d391e2d02a8bd0865951ba20a16719d7eb
mrkrca <aleksa.djuric.dj@gmail.com>
2025-01-07 20:59:29 +0100

    converting Fahrenheit to Celsius

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 console.log(process.env.API_KEY);
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
@@ -19,20 +19,30 @@ app.get("/", (req, res)=> {
     res.render("index.ejs", { content: "result"});
 })
 
-app.post("/", async (req, res)=> {
+app.post("/", async (req, res) => {
     try {
         console.log(req.body);
-        const cityChoice = req.body.choice
+        const unit = req.body.units;
+        console.log(req.body.units);
+
+        const cityChoice = req.body.choice;
         const response = await axios.get("http://api.weatherstack.com/current?" + "access_key=" + process.env.API_KEY + "&query=" + cityChoice);
-        const result = response.data
+        const result = response.data;
         console.log(JSON.stringify(result));
-        
-        res.render("index.ejs", { content: result});
-    } catch (error){
+
+        if (unit === "American") {
+            const celsiusTemp = result.current.temperature;
+            const fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
+            result.current.temperature = fahrenheitTemp.toFixed(2); // Convert to Fahrenheit and round to 2 decimal places
+            console.log(fahrenheitTemp);
+        }
+        res.render("index.ejs", { content: result, unit: unit });
+    } catch (error) {
         console.error(error);
-       
+        res.render("index.ejs", { content: null, error: "Error fetching weather data", unit: null });
     }
-})
+});
+
 
 app.listen(port, ()=> {
     console.log(`Server is listening to port ${port}`);
